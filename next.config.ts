f8b36585b5ae6fdda12d4fab2b8d76ddc7a5cd6e
Paulo Mariano <paulomariano2501@gmail.com>
2025-04-04 16:06:22 -0300

    Add .vercelignore and update build scripts for Prisma client generation
    - Introduced a new `.vercelignore` file to exclude the Prisma client from deployment.
    - Simplified the webpack configuration in `next.config.ts` for handling the Prisma client.
    - Updated `package.json` to streamline the build process by integrating Prisma generation into the build and vercel-build scripts.
    - Adjusted `vercel.json` to specify the build command directly without unnecessary builds configuration.
    - Modified Prisma schema to set the output path for the client generator.

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	webpack: (config, { isServer }) => {
 		if (isServer) {
-			config.externals.push({
-				"@prisma/client": "@prisma/client",
-			});
+			config.externals.push("@prisma/client");
 		}
 		return config;
 	},
-
 	output: "standalone",
-	experimental: {
-		serverComponentsExternalPackages: ["@prisma/client"],
-	},
 };
 
 export default nextConfig;
