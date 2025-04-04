ba6c75b67fd4c123b1b5b4cf715c18d44cd28d05
Paulo Mariano <paulomariano2501@gmail.com>
2025-04-04 13:55:52 -0300

    Remove console log from FindEmployeeUseCase to clean up code

--- src/domain/employees/use-cases/find-employee-use-case.ts
+++ src/domain/employees/use-cases/find-employee-use-case.ts
@@ -4,7 +4,6 @@ export class FindEmployeeUseCase {
 	constructor(private readonly employeeRepository: IEmployeeRepository) {}
 
 	async execute({ id }: { id?: string }) {
-		console.log(id);
 		return this.employeeRepository.find({ id });
 	}
 }
